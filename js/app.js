5992144b9ac5e3649346e23b4ddb7937acc5b0ad
doerofthings <doerofthings@gmail.com>
2022-02-03 23:16:57 -0500

    implemented the clear btn

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-// Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
-// Hint: “Hovering” is what happens when your mouse enters a div and ends when your mouse leaves it. You can set up event listeners for either of those events as a starting point.
-// There are multiple ways to change the color of the divs, including:
-// adding a new class to the div.
-// changing the div’s background color using JavaScript.
+// Add a button to the top of the screen which will clear the current grid and send the user a popup asking for the number of squares per side for the new grid. Once entered, the new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
+const topSectionContainer = document.querySelector('.top-section');
+const clearBtn = document.createElement('a');
+clearBtn.textContent = "Clear Grid";
+clearBtn.classList.add('clear-btn');
+topSectionContainer.append(clearBtn);
 
 let div = document.getElementById("grid-container");
-
-for (let i = 1; i < 257; i++) {
+let gridLength = 257
+for (let i = 1; i < gridLength; i++) {
     let span = document.createElement("span");
     span.setAttribute("class", "new");
     // span.innerHTML += i;
@@ -18,15 +19,18 @@ for (let i = 1; i < 257; i++) {
     boxArray.forEach(box =>{
         console.log("this is a box")
         box.addEventListener("mouseover", event =>{
-            box.classList.add('red');
-            console.log(document.querySelector('span.new.red'))
-            
-            
-            
+            box.classList.add('red');  
         })
-        box.addEventListener("mouseout", event =>{
-            box.classList.add('black')
-        })
+        clearBtn.addEventListener("click", event =>{
+            if(document.querySelector('span')){
+          
+               let remover = document.querySelectorAll('span.new.red');
+               box.classList.remove('red')
+            }
+         
+         })
+ 
     })
+
 }
 
